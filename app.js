9449ba60f556b03e83a98841beba4f815503b907
Loic Kartono <contact@kartono-loic.com>
2014-08-29 22:22:16 +1100

    Add demo for locale and add gitignore file

--- app.js
+++ app.js
@@ -21,6 +21,26 @@ angular.module('GooglePickerExample', ['lk-google-picker'])
   };
 })
 
-.controller('ExampleCtrl', ['$scope', function($scope) {
-  $scope.files = [];
+.controller('ExampleCtrl', ['$scope', 'lkGoogleSettings', function($scope, lkGoogleSettings) {
+  $scope.files     = [];
+  $scope.languages = [
+    { code: 'en', name: 'English' },
+    { code: 'fr', name: 'Français' },
+    { code: 'ja', name: '日本語' },
+    { code: 'ko', name: '한국' },
+  ]
+
+  // Check for the current language depending on lkGoogleSettings.locale
+  $scope.initialize = function() {
+    angular.forEach($scope.languages, function(language, index) {
+      if (lkGoogleSettings.locale === language.code) {
+        $scope.selectedLocale = $scope.languages[index];
+      }
+    });
+  }
+
+  // Define the locale to use
+  $scope.changeLocale = function(locale) {
+    lkGoogleSettings.locale = locale.code;
+  }
 }]);
