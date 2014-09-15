013454a0de53859941f0ba0a4580592f684cf2e0
Loic Kartono <contact@kartono-loic.com>
2014-09-16 10:34:41 +1100

    Update demo with commit cbcec12c333d01b384c87ada911b2dd9847db948

--- app.js
+++ app.js
@@ -5,13 +5,7 @@ angular.module('GooglePickerExample', ['lk-google-picker'])
   // Configure the API credentials here
   lkGoogleSettingsProvider.configure({
     apiKey   : 'AIzaSyAEu079vZFFeuFjpfWOrcmw2uGxISgmWwI',
-    clientId : '20787361493-372fi66o31k7t4t2ha3nvj5j36blm417.apps.googleusercontent.com',
-    scopes   : ['https://www.googleapis.com/auth/drive'],
-    features : ['MULTISELECT_ENABLED'],
-    views    : [
-      'DocsView().setIncludeFolders(true)',
-      'DocsUploadView().setIncludeFolders(true)'
-    ]
+    clientId : '20787361493-372fi66o31k7t4t2ha3nvj5j36blm417.apps.googleusercontent.com'
   });
 }])
 
