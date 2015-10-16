75e8c0498cce1b1d6341e79edf35be57e11f17fc
Loic Kartono <contact@kartono-loic.com>
2015-10-17 06:55:33 +1100

    Update to v0.2.2

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ angular.module('GooglePickerExample', ['lk-google-picker'])
     { code: 'fr', name: 'Français' },
     { code: 'ja', name: '日本語' },
     { code: 'ko', name: '한국' },
-  ]
+  ];
 
   // Check for the current language depending on lkGoogleSettings.locale
   $scope.initialize = function () {
@@ -31,15 +31,27 @@ angular.module('GooglePickerExample', ['lk-google-picker'])
         $scope.selectedLocale = $scope.languages[index];
       }
     });
+  };
+
+  // Callback triggered after Picker is shown
+  $scope.onLoaded = function () {
+    console.log('Google Picker loaded!');
   }
 
-  // Callback triggered after files selection
-  $scope.afterSelectCallback = function (files) {
-    alert("After Select Callback\n" + (files.length) + " file(s) selected");
+  // Callback triggered after selecting files
+  $scope.onPicked = function (docs) {
+    angular.forEach(docs, function (file, index) {
+      $scope.files.push(file);
+    });
+  }
+
+  // Callback triggered after clicking on cancel
+  $scope.onCancel = function () {
+    console.log('Google picker close/cancel!');
   }
 
   // Define the locale to use
   $scope.changeLocale = function (locale) {
     lkGoogleSettings.locale = locale.code;
-  }
+  };
 }]);
